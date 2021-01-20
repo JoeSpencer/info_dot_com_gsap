a4d90dac237fb996a8296f60aac7a86116cae1f2
Joe Spencer <spencerjoe@mac.com>
2021-01-20 09:08:15 -0600

    links

--- src/components/Home.js
+++ src/components/Home.js
@@ -182,7 +182,10 @@ class Home extends React.Component {
 
                     <div ref={(div) => (this.item5 = div)} className="item5">
                       <span class="inline-flex sm:ml-auto sm:mt-8 mt-24 justify-center sm:justify-start">
-                        <a class="text-gray-500">
+                        <a
+                          href="https://twitter.com/spencerjoe"
+                          class="text-gray-500"
+                        >
                           <svg
                             fill="currentColor"
                             stroke-linecap="round"
@@ -194,7 +197,10 @@ class Home extends React.Component {
                             <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                           </svg>
                         </a>
-                        <a class="ml-3 text-gray-500">
+                        <a
+                          href="https://www.instagram.com/spencerjoe/"
+                          class="ml-3 text-gray-500"
+                        >
                           <svg
                             fill="none"
                             stroke="currentColor"
@@ -215,7 +221,10 @@ class Home extends React.Component {
                             <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                           </svg>
                         </a>
-                        <a class="ml-3 text-gray-500">
+                        <a
+                          href="https://www.linkedin.com/in/joespencer2/"
+                          class="ml-3 text-gray-500"
+                        >
                           <svg
                             fill="currentColor"
                             stroke="currentColor"
